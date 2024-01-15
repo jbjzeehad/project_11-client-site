f18aef837f8b51341a9f8db09dc1926e0ec3b11a
jbjzeehad <ilamta.97@gmail.com>
2024-01-16 00:29:44 +0600

    setup and installation

---
+++ pet-adoption-client/src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB4noARKs9M3eB_aVeAXKI7mWuL2mqyoco",
+    authDomain: "pet-adoption-2b1db.firebaseapp.com",
+    projectId: "pet-adoption-2b1db",
+    storageBucket: "pet-adoption-2b1db.appspot.com",
+    messagingSenderId: "103107191230",
+    appId: "1:103107191230:web:de2ceb90aa30b741b419ef"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
